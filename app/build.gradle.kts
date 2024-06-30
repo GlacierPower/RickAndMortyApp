8b2c94e13c3d197b013e8bbec4f340b94eab0052
GlacierPower <114432349+GlacierPower@users.noreply.github.com>
2024-06-30 22:43:44 +0300

    Bottom navigation and navigation component were implemented.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,12 @@ dependencies {
     implementation(libs.androidx.hilt.work)
     implementation(libs.androidx.work.runtime.ktx)
 
+    // Navigation Components
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    implementation(project(":feature"))
+
     implementation(libs.androidx.core.ktx.v1101)
     implementation(libs.androidx.appcompat.v161)
     implementation(libs.material.v190)
